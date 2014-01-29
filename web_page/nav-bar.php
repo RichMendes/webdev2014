1f4ec1c7c4eb1f624c35e79656392bb28b29759f
mendes <richmendes@gmail.com>
2014-01-29 09:44:07 -0500

    Basic Website First Upload - Working

--- web_page/nav-bar.php
+++ web_page/nav-bar.php
@@ -8,35 +8,35 @@ echo '<div class="navbar navbar-inverse navbar-fixed-top" role="navigation">
             <span class="icon-bar"></span>
             <span class="icon-bar"></span>
        </button>
-       <a class="navbar-brand" href="http://localhost:8080/web_page/">Basic Website</a>
+       <a class="navbar-brand" href="./">Basic Website</a>
      </div>
         <div class="collapse navbar-collapse">
           <ul class="nav navbar-nav">';
-			if (basename($_SERVER['PHP_SELF']) == 'index.html')
-				echo  '<li class="active"><a href="http://localhost:8080/web_page/">Home</a></li>';
+			if (basename($_SERVER['PHP_SELF']) == 'index.php')
+				echo  '<li class="active"><a href="./index.php">Home</a></li>';
 			else
-				echo  '<li><a href="http://localhost:8080/web_page/">Home</a></li>';
-			if (basename($_SERVER['PHP_SELF']) == 'about.html')
-				echo  '<li class="active"><a href="http://localhost:8080/web_page/about.html">
+				echo  '<li><a href="./index.php">Home</a></li>';
+			if (basename($_SERVER['PHP_SELF']) == 'about.php')
+				echo  '<li class="active"><a href="./about.php">
 						About</a></li>';
 			else
-				echo  '<li><a href="http://localhost:8080/web_page/about.html">About</a></li>';
-			if (basename($_SERVER['PHP_SELF']) == 'contact.html')
-				echo  '<li class="active"><a href="http://localhost:8080/web_page/contact.html">
+				echo  '<li><a href="./about.php">About</a></li>';
+			if (basename($_SERVER['PHP_SELF']) == 'contact.php')
+				echo  '<li class="active"><a href="./contact.php">
 						Contact</a></li>';
 			else
-				echo  '<li><a href="http://localhost:8080/web_page/contact.html">Contact</a></li>';
-			if (basename($_SERVER['PHP_SELF']) == 'project1.html')
-				echo  '<li class="active"><a href="http://localhost:8080/web_page/project1.html">
+				echo  '<li><a href="./contact.php">Contact</a></li>';
+			if (basename($_SERVER['PHP_SELF']) == 'project1.php')
+				echo  '<li class="active"><a href="./project1.php">
 						Project One</a></li>';
 			else
-				echo  '<li><a href="http://localhost:8080/web_page/project1.html">
+				echo  '<li><a href="./project1.php">
 						Project One</a></li>';
-			if (basename($_SERVER['PHP_SELF']) == 'project2.html')
-				echo  '<li class="active"><a href="http://localhost:8080/web_page/project2.html">
+			if (basename($_SERVER['PHP_SELF']) == 'project2.php')
+				echo  '<li class="active"><a href="./project2.php">
 						Project Two</a></li>';
 			else
-				echo  '<li><a href="http://localhost:8080/web_page/project2.html">
+				echo  '<li><a href="./project2.php">
 						Project Two</a></li>';
      echo '</ul>
         </div><!--/.nav-collapse -->
