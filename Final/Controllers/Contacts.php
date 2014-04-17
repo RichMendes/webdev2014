8eef1a5205fd232dd6f01e6f164b65e501ca5d36
mendes <richmendes@gmail.com>
2014-04-17 07:46:08 -0400

    Update Contacts and start update on items

--- Final/Controllers/Contacts.php
+++ Final/Controllers/Contacts.php
@@ -10,13 +10,30 @@
 			$view = 'edit';
 			break;
 		case 'edit':
+			$model = Contacts::Get($_REQUEST['id']);
 			break;
 		case 'save':
-			//	TODO: Validate
-			Contacts::Create($_REQUEST);
-			$view = 'edit';
+			$sub_action = empty($_REQUEST['id']) ? 'created' : 'updated';
+			$errors = Contacts::Validate($_REQUEST);
+			if(!$errors){
+				$errors = Contacts::Save($_REQUEST);
+			}
+			if(!$errors){
+				header("Location: ?sub_action=$sub_action&id=$_REQUEST[id]");
+				die();
+			}else {
+				$model = $_REQUEST;
+				$view = 'edit';
+				
+			}
 			break;
 		case 'delete':
+			if($_SERVER['REQUEST_METHOD'] == 'GET'){
+				//prompt
+				$model = Contacts::Get($_REQUEST['id']);
+			} else{
+				$errors = Contacts::Delete($_REQUEST['id']);
+			}
 			break;
 		default:
 			$model = Contacts::Get();
@@ -24,6 +41,10 @@
 	}
 	
 	switch($format) {
+		case 'json':
+			$ret = array('success'=> empty($errors), 'errors'=> $errors, 'data'=> $model);
+			echo json_encode($ret);
+			break;
 		case 'plain':
 			include __DIR__ . "/../Views/Contacts/$view.php";
 			break;
--- Final/Controllers/Items.php
+++ Final/Controllers/Items.php
@@ -10,13 +10,30 @@
 			$view = 'edit';
 			break;
 		case 'edit':
+			$model = Items::Get($_REQUEST['id']);
 			break;
 		case 'save':
-			//	TODO: Validate
-			Items::Create($_REQUEST);
-			$view = 'edit';
+			$sub_action = empty($_REQUEST['id']) ? 'created' : 'updated';
+			$errors = Items::Validate($_REQUEST);
+			if(!$errors){
+				$errors = Items::Save($_REQUEST);
+			}
+			if(!$errors){
+				header("Location: ?sub_action=$sub_action&id=$_REQUEST[id]");
+				die();
+			}else {
+				$model = $_REQUEST;
+				$view = 'edit';
+				
+			}
 			break;
 		case 'delete':
+			if($_SERVER['REQUEST_METHOD'] == 'GET'){
+				//prompt
+				$model = Items::Get($_REQUEST['id']);
+			} else{
+				$errors = Items::Delete($_REQUEST['id']);
+			}
 			break;
 		default:
 			$model = Items::Get();
@@ -24,6 +41,10 @@
 	}
 	
 	switch($format) {
+		case 'json':
+			$ret = array('success'=> empty($errors), 'errors'=> $errors, 'data'=> $model);
+			echo json_encode($ret);
+			break;
 		case 'plain':
 			include __DIR__ . "/../Views/Items/$view.php";
 			break;
